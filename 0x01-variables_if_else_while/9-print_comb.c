18be21a8da15764f3b9145f466a872fb5af40bc3
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-14 16:35:25 -0700

    chitchi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,18 @@
  */
 int main(void)
 {
-
 	int x;
 
-	for (x = 48; x <= 57; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
+		if (x == '9')
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
 }
-
