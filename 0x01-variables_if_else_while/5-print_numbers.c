7294daf9fd5b32b9b8065b47deaa6461e49b20b7
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-14 14:21:35 -0700

    chitchi

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Discrp: 'prints all single digit numbers of base 10 starting from 0'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		printf("%d", ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
