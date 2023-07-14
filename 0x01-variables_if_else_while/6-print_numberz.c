e5503080e9267523230677cb7faf6484a9fb8f0b
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-14 14:39:55 -0700

    chitchi

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Discrp: 'prints all single digit numbers of base 10 starting from 0'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+		putchar (ch);
+	putchar('\n');
+	return (0);
+}
+
