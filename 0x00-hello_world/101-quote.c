896fe4204d8a3231364829e9eb682476daff5537
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-13 19:18:08 -0700

    chitchi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 3);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10);
 	return (1);
 }
