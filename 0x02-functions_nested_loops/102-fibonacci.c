3e2e35f31ebd3bd418e46e71a6abe46635944118
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-18 16:30:34 -0700

    chitchi

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+	unsigned long int1 = 0, int2 = 1, num;
+
+	for (x = 0; x < 50; x++)
+	{
+		num = int1 + int2;
+		printf("%lu", num);
+
+		int1 = int2;
+		int2 = sum;
+
+		if (x == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
+
