5a298f4f360deef1c5e82bcfe5bf7ec701873359
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-18 15:05:27 -0700

    chitchi

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - adds two integers and returns the result
+* @x: int  integers 1
+* @y: int  integers 2
+* Return: return integers 1  + integers 2
+ **/
+int add(int x, int y)
+{
+	return (x + y);
+}
+
