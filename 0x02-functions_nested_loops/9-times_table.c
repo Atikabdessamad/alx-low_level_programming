159ef75189c8f18baf65140147e89a4c3630ab6c
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-18 14:41:46 -0700

    chitchi

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,4 +14,22 @@ void times_table(void)
 			if (y == 0)
 			{
 				_putchar(z + '0');
+			}
+			if (z < 10 && y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
 
