dd053dce9a62fd944b72ecbc008b1f64c49810a2
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-20 04:20:11 -0700

    chitchi

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal -  draws a diagonal line on the terminal
+ * @n: int
+ * Return: Nothing
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (y = 1; y < i; y++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+
