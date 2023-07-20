f84fa13bfafc6e107313dbf0f97d7821a2aa8327
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-20 01:04:36 -0700

    chitchi

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Print number
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
+
