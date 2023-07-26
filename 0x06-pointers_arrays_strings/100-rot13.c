cf6d0b4b2199941f8cd4296f4d3eb68991b087bd
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-26 16:55:04 -0700

    m

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @a: str
+ * Return: void
+ */
+char *rot13(char *a)
+{
+	int x, y;
+	char tab0[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char tab1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; *(a + x); x++)
+	{
+		for (y = 0; tab0[y] != 0; y++)
+		{
+			if (*(a + x) == tab0[y])
+			{
+				*(a + x) = tab1[y];
+				continue;
+			}
+		}
+	}
+	return (a);
+}
