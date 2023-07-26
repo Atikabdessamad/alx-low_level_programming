7efa5efe4006db3daa8ac3ce9d46dbbb7c562893
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-07-26 15:43:42 -0700

    chitchi

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - Copie a string
+ * @s1: str 1
+ * @s2: str 2
+ * Return: void
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			x = ((int)*s1 - 48)
+			continue;
+		}
+		s1++;
+		s2++;
+	}
+	return (x);
+}
