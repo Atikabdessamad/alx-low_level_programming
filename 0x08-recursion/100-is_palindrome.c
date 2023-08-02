95b68b69e8d762097bca84122098e1b14f36639c
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-02 08:56:35 -0700

    ht

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,21 +14,6 @@ int check_palindrome(int start, int end, char *s)
 		return (1);
 	return (0);
 }
-/**
- * is_palindrome - Check str is palindrome.
- * @s: String
- * Return: Int
- */
-int is_palindrome(char *s)
-{
-	int y = _strlen_recursion(s) - 1;
-	int x = check_palindrome(0, y, s);
-
-	if (x == 0)
-		return (0);
-	else
-		return (1);
-}
 
 /**
  *_strlen_recursion - Return the length of  str
@@ -42,4 +27,19 @@ int _strlen_recursion(char *s)
 		return (0);
 	}
 	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * is_palindrome - Check str is palindrome.
+ * @s: String
+ * Return: Int
+ */
+int is_palindrome(char *s)
+{
+	int x = _strlen_recursion(s) - 1;
+	int y = check_palindrome(0, y, s);
+
+	if (y == 0)
+		return (0);
+	else
+		return (1);
 }
