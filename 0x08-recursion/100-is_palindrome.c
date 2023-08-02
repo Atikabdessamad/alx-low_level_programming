80fa11ec3df353ceafdddd6cc09042f31526c330
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-02 08:52:58 -0700

    ht

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,7 @@
  */
 int check_palindrome(int start, int end, char *s)
 {
-	if (*(s + start) == *(s + end)
+	if (*(s + start) == *(s + end))
 		return (1 * check_palindrome(start + 1, end - 1, s));
 	if (start == end || start - 1 == end)
 		return (1);
@@ -21,8 +21,8 @@ int check_palindrome(int start, int end, char *s)
  */
 int is_palindrome(char *s)
 {
-	int x = check_palindrome(0, len, s);
 	int y = _strlen_recursion(s) - 1;
+	int x = check_palindrome(0, y, s);
 
 	if (x == 0)
 		return (0);
@@ -43,4 +43,3 @@ int _strlen_recursion(char *s)
 	}
 	return (1 + _strlen_recursion(s + 1));
 }
-
