14068b81bef8d448512c044fc9ec2cbbedc0e582
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-04 06:20:10 -0700

    chi

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 	int x, y, z = 0;
 	int num[5] = {25, 10, 5, 2, 1};
 
-	if  (atoi(argv[1]) < 0)
-	{
-		printf("0\n");
-	}
-	else if (argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
 	}
 	else if (atoi(argv[1]) >= 0)
 	{
