c31dd637d0741a0fc3f8295f19be3fe7a0b14ea5
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-07 09:20:04 -0700

    cjitchi

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,10 +7,34 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *p;
-	(void)ac;
-	(void)av;
+	int x, y, z, v = 0, w = 0;
+	char *ptr;
 
-	p = "R";
-	return (p);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+			v++;
+		v++;
+	}
+
+	ptr = malloc(sizeof(char) * (v + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (x = 0; x < ac; x++)
+	{
+		for (z = 0; av[x][z] != '\0'; z++)
+		{
+			ptr[w] = av[x][z];
+			w++;
+		}
+		ptr[w] = '\n';
+		w++;
+	}
+	ptr[w] = '\0';
+
+	return (ptr);
 }
