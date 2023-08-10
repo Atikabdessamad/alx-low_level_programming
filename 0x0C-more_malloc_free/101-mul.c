17faedeca23c67c773e5625dc3ea70063e170433
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-10 13:45:18 -0700

    a

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,35 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
-* is_valid_num - Check if a string contains only digits
-* @tab: str
-* Return: void
+* is_valid_number - Check if a string contains only digits.
+* @str: string.
+* Return: 1.
 */
-int is_valid_num(char *tab)
+int is_valid_number(char *str)
 {
-	int x = 0;
+	int i = 0;
 
-	while (tab[x])
+	while (str[i])
 	{
-		if (tab[x] < '0' || tab[x] > '9')
+		if (str[i] < '0' || str[i] > '9')
 			return (0);
-		x++;
+		i++;
 	}
 
 	return (1);
 }
-
 /**
-* main - multiplies two positive numbers
-* @argc: int
-* @argv: str
-* Return: void
+* main - Multiply two arguments passed to the program.
+* @argc: The number of arguments.
+* @argv: The array of argument values.
+* Return: 0 on success.
 */
 int main(int argc, char **argv)
 {
-	unsigned long x1, x2, res;
+	unsigned long num1, num2, result;
 
 	if (argc != 3)
 	{
@@ -37,18 +35,17 @@ int main(int argc, char **argv)
 		return (98);
 	}
 
-	if (!is_valid_num(argv[1]) || !is_valid_num(argv[2]))
+	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	x1 = strlen(argv[1]);
-	x2 = strlen(argv[2]);
-	res = x1 * x2;
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
+	result = num1 * num2;
 
-	printf("%lu\n", res);
+	printf("%lu\n", result);
 
 	return (0);
-
 }
