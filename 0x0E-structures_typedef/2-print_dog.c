c0cce4e9b9f915aa165b09e9abe0b129d52ec052
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-14 05:38:04 -0700

    chitch

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,20 +7,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
 		printf("Owner %s\n", d->owner ? d->owner : "(nil)");
 	}
-}
-int main(void)
-{
-	struct dog my_dog;
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	print_dog(&my_dog);
-
-	return (0);
 }
