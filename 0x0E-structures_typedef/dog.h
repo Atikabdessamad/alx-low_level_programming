52324561809ce10579694c838bcaf36709b6e602
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-14 04:31:24 -0700

    chitch

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure for Dog.
+ * @name: String, name the dog
+ * @age: Float, age the gog
+ * @owner: String, owner name
+ *
+ * Descriotion: Define new type struct dog with name, age, owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
