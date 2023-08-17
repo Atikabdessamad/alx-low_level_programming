aa66c82d216ea8be870136c323064eab7ac0334b
Atikabdessamad <Atikabdessamad24@gmail.com>
2023-08-17 08:25:58 -0700

    c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+* print_strings - prints strings
+*@separator: separator
+*@n: num
+*@...: str
+*Return: void
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+va_list x;
+unsigned int y;
+char *ptr;
+
+va_start(x, n);
+
+for (y = 0; y < n; y++)
+{
+str = va_arg(x, char *);
+if (ptr == NULL)
+printf("(nil)");
+else
+printf("%s", ptr);
+
+if (y != (n - 1) && separator != NULL)
+printf("%s", separator);
+
+}
+
+printf("\n");
+va_end(x);
+}
