8f0eb7b143393f600faf5cec4cdb9794cb5dc9ec
Atikabdessamad <atikabdessamad24@gmail.com>
2023-08-28 09:47:37 -0700

    c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: ptr
+ *
+ * Return: cen
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t cen = 0;
+	int def;
+	listint_t *tmp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		def = *h - (*h)->next;
+		if (def > 0)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+			cen++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			cen++;
+			continue;
+		}
+	}
+
+	*h = NULL;
+
+	return (cen);
+}
