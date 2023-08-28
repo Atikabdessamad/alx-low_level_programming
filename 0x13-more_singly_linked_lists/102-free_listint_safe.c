74450c800ab894d98fcc731c0320efc31a4c56a6
Atikabdessamad <atikabdessamad24@gmail.com>
2023-08-28 09:50:18 -0700

    c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,39 +2,39 @@
 
 /**
  * free_listint_safe - frees a linked list
- * @h: ptr
+ * @h: pointer to the first node in the linked list
  *
- * Return: cen
+ * Return: number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t cen = 0;
-	int def;
-	listint_t *tmp;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
 	if (!h || !*h)
 		return (0);
 
 	while (*h)
 	{
-		def = *h - (*h)->next;
-		if (def > 0)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			tmp = (*h)->next;
+			temp = (*h)->next;
 			free(*h);
-			*h = tmp;
-			cen++;
+			*h = temp;
+			len++;
 		}
 		else
 		{
 			free(*h);
 			*h = NULL;
-			cen++;
+			len++;
 			break;
 		}
 	}
 
 	*h = NULL;
 
-	return (cen);
+	return (len);
 }
